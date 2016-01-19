c4a83aca0e095c78fdd9698bb07c89125b365e40
Dilip <dilip@artoo.in>
2016-01-19 20:23:34 +0530

    basic version ready with routing fixed.

--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params, transition){
+  	let posts = this.modelFor('posts');
+  	let id = +params.id;
+    let [post] = posts.filterBy('id', id);
+  	return post;
+  }
 });
