d332175be4f8762a348cd1a033dfb2627878006f
Dilip <dilip@artoo.in>
2016-01-20 23:05:04 +0530

    spelling fix

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function (params, transition) {
     return [
       {id: 1, title: 'Welcome to my blog', content: 'hello everyone, welcome to my blog'},
-      {id: 2, title: 'A trip to remmeber', content: 'We went to a beautiful place this summar'}
+      {id: 2, title: 'A trip to remmeber', content: 'We went to a beautiful place this summer'}
     ];
   }
 });
